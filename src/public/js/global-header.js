52bb8bf70561739071a7122869ddf013028721fe
philliephanatic <nolan.r.young@gmail.com>
2025-12-02 17:03:22 -0500

    added: basic structure of index.ejs, head.ejs, migrated header.ejs, footer.ejs, global-header.js, LinkedIn logo & GitHub logo (to img folder) from previous version (titled: portfolio website)

---
+++ src/public/js/global-header.js
@@ -0,0 +1,12 @@
+// Mobile nav
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger-menu");
+  const mainNav = document.querySelector(".main-nav");
+
+  if (hamburger && mainNav) {
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      mainNav.classList.toggle("active");
+    });
+  }
+});
