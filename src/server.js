215dd96e50223eb5371919e18b38558ee14fc1ab
philliephanatic <nolan.r.young@gmail.com>
2025-12-05 16:01:52 -0500

    continued work on porting over files and code from portfolio-website. CSS has correctly been connected to HTML; had to adjust STATIC FILES code in src/server.js

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Static assets
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', router);
